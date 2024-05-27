9a2a51d54c481be8f93ad2b5c7b35ed0cf6ec449
Dhruv Panchal <m3dhruv@gmail.com>
2024-05-27 08:29:38 +0530

    Take command form user

--- 1-DESKTOP-ASSISTANCE.py
+++ 1-DESKTOP-ASSISTANCE.py
@@ -22,8 +22,25 @@ def wishMe():
 
     speak("I am Jarvis. Please tell me how can i help you !")
 
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
 
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language='en-in')
+            print(f"User said: {query}\n")
 
+        except Exception as e:
+            print(e)
+            print("Say that again please...")
+            return "None"
+        return query
 
 if __name__ == "__main__":
     wishMe()
+    while True:
+        query = takeCommand().lower()
