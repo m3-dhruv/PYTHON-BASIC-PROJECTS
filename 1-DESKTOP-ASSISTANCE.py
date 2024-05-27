2ffc330688868cd76626094dd546737f0f87dbbb
Dhruv Panchal <m3dhruv@gmail.com>
2024-05-27 08:39:41 +0530

    Wikipedia

--- 1-DESKTOP-ASSISTANCE.py
+++ 1-DESKTOP-ASSISTANCE.py
@@ -2,6 +2,7 @@
 import pyttsx3 # type: ignore
 import speech_recognition as sr  # type: ignore
 import datetime
+import wikipedia
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -44,3 +45,11 @@ if __name__ == "__main__":
     wishMe()
     while True:
         query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
